19bbbf11ec0d0df20fb471a81fee08322d2bc1c1
Shalini <shalini.n@isteer.com>
2021-04-20 10:12:03 +0530

    new activity

---
+++ activity/Addition/activity.go
@@ -0,0 +1,46 @@
+package Addition
+
+import (
+	_ "github.com/influxdata/influxdb1-client" // this is important because of the bug in go mod
+	"github.com/project-flogo/core/activity"
+)
+
+func init() {
+	_ = activity.Register(&Activity{}) //activity.Register(&Activity{}, New) to create instances using factory method 'New'
+}
+
+//var activityMd = activity.ToMetadata(&Input{}, &Output{})
+var activityMd = activity.ToMetadata(&Input{}, &Output{})
+
+//New optional factory method, should be used if one activity instance per configuration is desired
+func New(ctx activity.InitContext) (activity.Activity, error) {
+
+	act := &Activity{} //add aSetting to instance
+
+	return act, nil
+}
+
+// Activity is an sample Activity that can be used as a base to create a custom activity
+type Activity struct {
+}
+
+// Metadata returns the activity's metadata
+func (a *Activity) Metadata() *activity.Metadata {
+	return activityMd
+}
+
+// Eval implements api.Activity.Eval - Logs the Message
+func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
+
+	input := &Input{}
+
+	out := input.Num1 + input.Num2
+
+	output := &Output{Output: out}
+	err = ctx.SetOutputObject(output)
+	if err != nil {
+		return true, err
+	}
+
+	return true, nil
+}
---
+++ activity/Addition/metadata.go
@@ -0,0 +1,46 @@
+package Addition
+
+import (
+	"fmt"
+
+	"github.com/project-flogo/core/data/coerce"
+	//"github.com/spf13/cast"
+)
+
+type Input struct {
+	Num1 int `md:"Num1.required"`
+	Num2 int `md:"Num2.required"`
+}
+
+func (r *Input) FromMap(values map[string]interface{}) error {
+
+	Val1, _ := coerce.ToInt(values["Num1"])
+	r.Num1 = Val1
+	fmt.Println(values["Num1"])
+	Val2, _ := coerce.ToInt(values["Num2"])
+	r.Num2 = Val2
+	return nil
+}
+
+func (r *Input) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"Num1": r.Num1,
+		"Num2": r.Num2,
+	}
+}
+
+type Output struct {
+	Output int `md:"Output"`
+}
+
+func (o *Output) FromMap(values map[string]interface{}) error {
+	strVal, _ := coerce.ToInt(values["Output"])
+	o.Output = strVal
+	return nil
+}
+
+func (o *Output) ToMap() map[string]interface{} {
+	return map[string]interface{}{
+		"Output": o.Output,
+	}
+}
